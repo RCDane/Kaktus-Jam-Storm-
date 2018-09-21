72f282fe0a9bd6e19b49e6f48bae54a2cb7eb168
Unknown <vez64ryj@gmail.com>
2018-09-21 21:46:17 +0200

    Added player input and movement

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    PlayerMovement playerMovement;
+	// Use this for initialization
+	void Start () {
+        playerMovement = GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        playerMovement.Move(xInput);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+    Rigidbody2D playerRB;
+    private float XInput;
+    private void FixedUpdate()
+    {
+        playerRB.AddForce(XInput * moveSpeed * Time.deltaTime*Vector2.right,ForceMode2D.Impulse);
+    }
+
+    public void Move(float x)
+    {
+        XInput = x;
+    }
+
+}
