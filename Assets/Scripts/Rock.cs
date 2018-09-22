9e6d1b5f3f17a8ab91c6017b993701a53695982a
Mikkelol <soml@itu.dk>
2018-09-22 16:09:05 +0200

    push and pull done

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -7,23 +7,34 @@ public class Rock : MonoBehaviour {
 
     // Use this for initialization
     private Rigidbody2D RigBod;
-
+    BoxCollider2D boxCollider;
+    [SerializeField] LayerMask rockLayer;
     void Start () {
         RigBod = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(RigBod.bodyType == RigidbodyType2D.Dynamic) {
-            if(Input.GetKey("e")) {
+        DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
+
+        if(Input.GetKey("w")) {
+            Destroy(joint);
+        } else if(Input.GetKey("e") ^ joint != null) {
+            if(joint != null) {
+                Destroy(joint);
+            } else {
                 var o = GameObject.FindGameObjectWithTag("Player");
-                //transform.position = Vector2.MoveTowards(transform.position,o.transform.position,0.5f);
-                RigBod.AddForce(o.transform.position - transform.position,ForceMode2D.Impulse);
-                
+                if((o.transform.position - transform.position).magnitude <= 1.5f) {
+                    joint = gameObject.AddComponent<DistanceJoint2D>();
+                    joint.autoConfigureDistance = false;
+                    joint.distance = 1.2f;
+                    joint.connectedBody = o.gameObject.GetComponent<Rigidbody2D>();
+                }
             }
-            
+
         }
-            
+
     }
 
     void OnCollisionEnter2D(Collision2D col) {
@@ -31,13 +42,6 @@ public class Rock : MonoBehaviour {
             Debug.Log("With Player!");
             var RB = GetComponent<Rigidbody2D>();
             RB.bodyType = RigidbodyType2D.Dynamic;
-            RigBod.velocity = Vector2.zero;
-        }
-    }
-    void OnCollisionStay(Collision col) {
-        if(col.gameObject.CompareTag("Player")) {
-            Debug.Log("Still here!");
-            RigBod.velocity = Vector2.zero;
         }
     }
 }
