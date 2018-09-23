bd38881fef989407133a642e8ddb7d9ccd55bc65
Unknown <vez64ryj@gmail.com>
2018-09-23 11:32:45 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+    BoxCollider2D collider;
+    [SerializeField]
+    private SpriteRenderer lightningStrike;
+    [SerializeField]
+    private SpriteRenderer Background;
+    [SerializeField]
+    private float lightningStrikeFadeSpeed;
+    [SerializeField]
+    private float backgroundFadeSpeed;
+
+    bool hasFired;
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasFired)
+            return;
+        else if(collision.transform.CompareTag("Player"))
+        {
+            Color newBack = Background.color;
+            newBack.a = 1;
+            Background.color = newBack;
+            Color newLight = lightningStrike.color;
+            newLight.a = 1;
+            lightningStrike.color = newLight;
+            StartCoroutine(Fade(Background,backgroundFadeSpeed));
+            StartCoroutine(Fade(lightningStrike, lightningStrikeFadeSpeed));
+        }
+    }
+
+
+    IEnumerator Fade(SpriteRenderer sprite, float t)
+    {
+        float time = Time.timeSinceLevelLoad;
+        while (Time.timeSinceLevelLoad < time + t)
+        {
+            float timeCalc = (Time.timeSinceLevelLoad - time) / (t);
+            Color c = sprite.color;
+            c.a = 1 - timeCalc;
+            sprite.color = c;
+            yield return null;
+        }
+        sprite.enabled = false;
+    }
+}
